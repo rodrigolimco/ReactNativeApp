50018189cbe22c95d4284286df31a926f857f3e1
Rodrigo Limpias <rodrigolimpiascossio@hotmail.com>
2017-12-12 22:39:17 +0100

    HousesList added; using webservice to get data

--- src/sections/houses/HousesList.js
+++ src/sections/houses/HousesList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, Button } from 'react-native'
 import axios from 'axios'
 
 export default class HousesList extends Component {
@@ -7,24 +7,46 @@ export default class HousesList extends Component {
     constructor(props) {
         super(props)
         this.state = {
-          list: []
+          list: [],
+          selecte: null
         }
     }
     
     componentWillMount() {
+
         axios.get('http://146.185.137.85/got/web/casas')
-        .then(function (response) {
+        .then((response) => {
           console.log("axios get response: ", response);
+          const nuestraLista = response.data && response.data.records ? response.data.records : []
+          this.setState({ list : nuestraLista })
         })
-        .catch(function (error) {
+        .catch((error) => {
             console.log("axios get error: ", error);
         });
     }
 
+    pintarCelda(item) {
+        return (
+            <View style={{height: 200, backgroundColor: 'grey', marginVertical: 10}}>
+                <Text>{ item.nombre }</Text>
+
+                <Button
+                    title={'Pulsa y te saco un log, a lo loco!'}
+                    onPress={ () => this.setState({ selected: item})}
+                />
+            </View>
+        )
+    }
+
     render() {
+        const nombre = this.state.selected ? this.state.selected.nombre : ''
         return (
             <View>
-                <Text>{ this.state.texto }</Text>
+                <Text>{ nombre }</Text>
+                <FlatList
+                data={ this.state.list }
+                renderItem={ ({ item }) => this.pintarCelda(item)}
+                />
             </View>
         )
     }
